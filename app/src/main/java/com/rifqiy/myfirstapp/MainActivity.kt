ad854af1b99108c1c878409659ca648483b3a86a
Development <rifkihamid77@gmail.com>
2020-03-15 17:31:42 +0700

    section two video three

--- app/src/main/java/com/rifqiy/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/rifqiy/myfirstapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rifqiy.myfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -17,6 +18,14 @@ class MainActivity : AppCompatActivity() {
             Log.i("Mainactivity","Button Was Click")
 
             Toast.makeText(this,"Button Was Clicked !",Toast.LENGTH_LONG).show()
+        }
+
+        btnSendMsgToNectActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
